3df80b2a42aec58c969971c7d99846f58f481783
andrii_ivantsiv <5127372+andrii-ivantsiv@users.noreply.github.com>
2023-04-01 22:22:08 +0200

    Update formatting solution for binary search exercise

--- leetcode-problems/src/binary/search/Solution.java
+++ leetcode-problems/src/binary/search/Solution.java
@@ -2,27 +2,27 @@ package binary.search;
 
 /**
  * 704. Binary Search
- *
+ * <p>
  * Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
- *
+ * <p>
  * You must write an algorithm with O(log n) runtime complexity.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: nums = [-1,0,3,5,9,12], target = 9
  * Output: 4
  * Explanation: 9 exists in nums and its index is 4
  * Example 2:
- *
+ * <p>
  * Input: nums = [-1,0,3,5,9,12], target = 2
  * Output: -1
  * Explanation: 2 does not exist in nums so return -1
- *
- *
+ * <p>
+ * <p>
  * Constraints:
- *
+ * <p>
  * 1 <= nums.length <= 104
  * -104 < nums[i], target < 104
  * All the integers in nums are unique.
@@ -38,7 +38,7 @@ public class Solution {
 
     public static int search(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i] == target) {
+            if (nums[i] == target) {
                 return i;
             }
         }
